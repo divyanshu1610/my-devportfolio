b9343d3d655eaff4d3e610955c099ec6fe3488f9
Divyanshu Maurya <divyanshu1610@gmail.com>
2020-08-11 02:54:15 +0530

    Updates

---
+++ precache-manifest.3307af5c6726e83e08d3c39d25105e1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37a8ec2d3c7f6174e4b0e89f45368150",
+    "url": "/my-devportfolio/index.html"
+  },
+  {
+    "revision": "46d0367c2212d5d03200",
+    "url": "/my-devportfolio/static/css/main.8124698e.chunk.css"
+  },
+  {
+    "revision": "9121dd774484663e1f86",
+    "url": "/my-devportfolio/static/js/2.33412e28.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/my-devportfolio/static/js/2.33412e28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46d0367c2212d5d03200",
+    "url": "/my-devportfolio/static/js/main.3431e072.chunk.js"
+  },
+  {
+    "revision": "a0fcd8205f51404cc0dd",
+    "url": "/my-devportfolio/static/js/runtime-main.2ce12770.js"
+  }
+]);
